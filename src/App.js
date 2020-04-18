9162a76d7db825115dea58c246c172a810a91190
Davy <davyfungaloi@gmail.com>
2020-04-18 19:57:56 +0200

    started on the recipelist

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import React, {useState} from 'react';
-import Counter from './Counter'
-import CounterHooks from './CounterHooks'
-
-export const ThemeContext = React.createContext()
+import React from 'react';
+import Recipelist from './RecipeList'
+import Ingredient from './Ingredient';
 
 function App() {
-  const [theme, setTheme] = useState('red')
   return (
-    <ThemeContext.Provider value={{ backgroundColor: theme}}>
-    Counter
-   <Counter initialCount = {0} />
-   CounterHooks
-   <CounterHooks initialCount = {0} />
-   <button onClick = {() => setTheme(PrevTheme => {
-    return PrevTheme === 'red' ? 'blue': 'red'})}
-    >Toggle Theme</button>
-
-   </ThemeContext.Provider>
+    <Recipelist recipes = {SampleRecipes}/>
   )
 }
+
+const SampleRecipes = [{
+  id: 1,
+  name: 'Plain Chicken',
+  servings: 3,
+  cookTime: '1:45',
+  instructions: "1. Put salt on chicken\n2. Put chicken in oven\n3. Eat chicken",
+  ingredients: [{
+    id: 1,
+    name: "Chicken",
+    amount: "2 pounds"
+
+  },{
+    id: 2,
+    name: "Salt",
+    amount: "1 tbs",
+    }
+  ]
+},
+{
+  id: 2,
+  name: 'Plain Pork',
+  servings: 5,
+  cookTime: '0:45',
+  instructions: "1. Put parprika on pork\n2. Put pork in oven\n3. Eat pork",
+  ingredients: [{
+    id: 1,
+    name: "Pork",
+    amount: "3 Pounds"
+
+  },{
+    id: 2,
+    name: "Paprika",
+    amount: "2 tbs",
+    }
+  ]
+
+}]
 
 export default App;
